9acacf71f3b6fb36e2ab69048e3ceafaa599a853
Luigi Savinelli <l.savinelli@gmail.com>
2016-07-25 10:38:26 +0200

    integration mailgun

--- slack_bot.js
+++ slack_bot.js
@@ -474,10 +474,9 @@ controller.hears(['^!mailmegaming (.*)'], 'direct_message,direct_mention,mention
 				  toSend = toSend + '\n' + removeLinkFormatting(reply[i]);
 				}
 			}
-			console.log(mailaddress);
 			nodemailerMailgun.sendMail({
 				  from: 'postmaster@sandboxad9fe307c5d64e09b4730761129a9fa7.mailgun.org',
-				  to: mailaddress, // An array if you have multiple recipients.
+				  to: removeLinkFormatting(mailaddress), // An array if you have multiple recipients.
 				  subject: 'Gaming News!',
 				  //You can use "html:" to send HTML email content. It's magic!
 				  html: toSend
